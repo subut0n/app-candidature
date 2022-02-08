ba44bba21cd6930311202dfdaa544707d2c514e4
Steven <stevendruesne@gmail.com>
2022-02-08 16:11:28 +0100

    Update front

--- App/routes.py
+++ App/routes.py
@@ -1,3 +1,4 @@
+import imp
 from flask import render_template, redirect, url_for, flash, request
 from App import db, app
 from datetime import date
@@ -5,6 +6,7 @@ from .models import Users, Candidacy
 from .forms import Login, AddCandidacy, ModifyCandidacy, ModifyProfile
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
+from .utils import isAsciiNumber
 
 @app.route('/')
 @app.route('/home')
