12980295fd5b4fd22e10cf1a9f61375fb845d733
theotrc <theotricot12@gmail.com>
2022-02-10 16:43:32 +0100

    lien mail

--- App/routes/routemail.py
+++ App/routes/routemail.py
@@ -1,22 +1,24 @@
 from App import db, app
 from flask_mail import Mail, Message
-from flask import Flask
-from ..infoMail import adresseMail, password
+from flask import Flask, request
+from ..infoMail import EMAIL_ADDRESS, EMAIL_PASSWORD
+import cherrypy
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = adresseMail
-app.config['MAIL_PASSWORD'] = password
+app.config['MAIL_USERNAME'] = EMAIL_ADDRESS
+app.config['MAIL_PASSWORD'] = EMAIL_PASSWORD
 app.config['MAIL_USE_SSL'] = False
 
 mail = Mail(app)
 
-@app.route('/send')
-def send():
-    msg = Message('sujet demon mail', sender='simploncandidature@gmail.com',
-            recipients=['a.dzikowski@outlook.fr'])
+@app.route('/send/<email>')
+def send(email):
+    msg = Message('sujet demon mail', sender= EMAIL_ADDRESS,
+            recipients=[email])
+    varlink = 'http://127.0.0.1:5000/modify_password/' + email
     
-    msg.html = 'Changer votre mot de passe <button class="pwd_forget"> <a href = "https://www.youtube.com/watch?v=J5bIPtEbS0Q&t=114s"> click here </a> </button>'
+    msg.body = "Changer votre mot de passe \n http://127.0.0.1:5000/modify_password/" + email
     mail.send(msg)
     return "message envoyé"
--- App/utils.py
+++ App/utils.py
@@ -1,6 +1,13 @@
+from App.models import Users
+
 def isAsciiNumber(mystr):
     mylist = ['0','1','2','3','4','5','6','7','8','9']
     for c in mystr:
         if c not in mylist:
             return False
     return True
+
+
+list_mail = [ c.email_address
+          
+               for c in Users.query]
