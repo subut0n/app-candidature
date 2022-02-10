2042c08b472407ccbc3cb821fefda472226e9bac
subut0n <a.dzikowski@outlook.fr>
2022-02-10 14:12:33 +0100

    modif config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import psycopg2
 # else:
 #     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
-SQLALCHEMY_DATABASE_URI = "postgres://zknfzgayureaep:3ea358f223f36b642b214319c7e095e787ae9a618cce330fe23065f5f12a3944@ec2-54-77-182-219.eu-west-1.compute.amazonaws.com:5432/d9blfsn01rfeds"
+SQLALCHEMY_DATABASE_URI = "postgresql://zknfzgayureaep:3ea358f223f36b642b214319c7e095e787ae9a618cce330fe23065f5f12a3944@ec2-54-77-182-219.eu-west-1.compute.amazonaws.com:5432/d9blfsn01rfeds"
 
 SECRET_KEY = 'VerySecret'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
