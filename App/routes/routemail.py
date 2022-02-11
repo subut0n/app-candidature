ac59252eb297e18af06da22bdaae0f7d95ff099b
subut0n <a.dzikowski@outlook.fr>
2022-02-11 10:59:36 +0100

    route_mail modifications

--- App/routes/routemail.py
+++ App/routes/routemail.py
@@ -1,14 +1,14 @@
 from App import db, app
 from flask_mail import Mail, Message
 from flask import Flask, request
-from ..infoMail import EMAIL_ADDRESS, EMAIL_PASSWORD
+from ..info_mail import EMAIL_ADDRESS, EMAIL_PASSWORD
 import cherrypy
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-#app.config['MAIL_USERNAME'] = EMAIL_ADDRESS
-#app.config['MAIL_PASSWORD'] = EMAIL_PASSWORD
+app.config['MAIL_USERNAME'] = EMAIL_ADDRESS
+app.config['MAIL_PASSWORD'] = EMAIL_PASSWORD
 app.config['MAIL_USE_SSL'] = False
 
 mail = Mail(app)
--- App/utils.py
+++ App/utils.py
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 from App.models import Users
-=======
-
->>>>>>> 1f59a014b846477a57cac5fbe8a2566ec5b79be9
 
 def isAsciiNumber(mystr):
     mylist = ['0','1','2','3','4','5','6','7','8','9']
     for c in mystr:
         if c not in mylist:
             return False
-<<<<<<< HEAD
     return True
 
 
-list_mail = [ c.email_address
-          
-               for c in Users.query]
-=======
-    return True
->>>>>>> 1f59a014b846477a57cac5fbe8a2566ec5b79be9
+list_mail = [ c.email_address for c in Users.query]
