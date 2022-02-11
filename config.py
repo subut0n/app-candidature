cb9fa62f28e6ab2f36dd433834d7802a5ab69a90
Steven <stevendruesne@gmail.com>
2022-02-11 14:52:27 +0100

    change uri

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 import psycopg2
 # Database initialization
-if os.environ.get('DATABASE_URL') is None:
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# if os.environ.get('DATABASE_URL') is None:
+#     basedir = os.path.abspath(os.path.dirname(__file__))
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# else:
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
-# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 SECRET_KEY = 'VerySecret'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
