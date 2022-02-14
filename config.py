5406d5cc9672aa938741c3aeb01b12f43a937c7f
Steven <stevendruesne@gmail.com>
2022-02-14 11:35:37 +0100

    Modified front of details page and home page

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 import psycopg2
 # Database initialization
-#if os.environ.get('DATABASE_URL') is None:
+# if os.environ.get('DATABASE_URL') is None:
 #    basedir = os.path.abspath(os.path.dirname(__file__))
 #    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-#else:
+# else:
 #    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
